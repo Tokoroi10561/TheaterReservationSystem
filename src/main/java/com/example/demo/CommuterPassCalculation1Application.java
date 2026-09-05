bfaacea1bbef2cac200cd7e3cf3e2deea62efe39
小林瑞歩 <mizuho.kobayashi@nkaihatsu.com>
2026-09-05 16:20:20 +0900

    create: 予約管理アプリ作成開始

---
+++ src/main/java/com/example/demo/CommuterPassCalculation1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuterPassCalculation1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuterPassCalculation1Application.class, args);
+	}
+
+}
