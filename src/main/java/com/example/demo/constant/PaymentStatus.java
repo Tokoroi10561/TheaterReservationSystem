a1167920e1949ab837f882ee2e0290b93994515a
小林瑞歩 <mizuho.kobayashi@nkaihatsu.com>
2026-09-08 22:33:39 +0900

    add: PaymentStatusの追加 [PENDING, COMPLETED, FAILED, REFUNDED]

--- src/main/java/com/example/demo/constant/PaymentStatus.java
+++ src/main/java/com/example/demo/constant/PaymentStatus.java
@@ -1,5 +1,17 @@
 package com.example.demo.constant;
 
-public class PaymentStatus {
+import lombok.Getter;
 
+@Getter
+public enum PaymentStatus {
+	PENDING("未払い"),
+	COMPLETED("支払い済み"),
+	FAILED("支払い失敗"),
+	REFUNDED("返金済み");
+
+	private final String label;
+
+	PaymentStatus(String label) {
+		this.label = label;
+	}
 }
