0fa7a1ff828afbcdee280da594cb81ca7889b29d
小林瑞歩 <mizuho.kobayashi@nkaihatsu.com>
2026-09-09 16:32:48 +0900

    add: ShowRepositoryの追加 [findByTitle]

---
+++ src/main/java/com/example/demo/repository/ShowRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.entity.Show;
+
+public interface ShowRepository extends JpaRepository<Show, Long> {
+	
+	@Query("SELECT s FROM Show s WHERE s.title LIKE %:title%")
+	List<Show>findByTitle(String title);
+}
