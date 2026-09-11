80b13c4856f2f19fc11013491e9c6a14c79fc265
小林瑞歩 <mizuho.kobayashi@nkaihatsu.com>
2026-09-11 21:17:26 +0900

    commentout: Serviceより前にDtoを作るべきと判断し、dtoの作成にシフト

--- src/main/java/com/example/demo/service/BookingService.java
+++ src/main/java/com/example/demo/service/BookingService.java
@@ -1,29 +1,29 @@
-package com.example.demo.service;
-
-import java.util.List;
-import java.util.Optional;
-
-import com.example.demo.entity.Booking;
-
-public interface BookingService {
-	
-	void createBooking(Booking booking);
-	
-	void updateBooking(Booking booking);
-	
-	void deleteBooking(Long Id);
-	
-	List<Booking> getAllBookings();
-	
-	List<Booking> getBookingsById(Long Id);
-	
-	List<Booking> getBookingsByName(String name);
-	
-	List<Booking> getBookingsByEmail(String email);
-	
-	List<Booking> getBookingsByStageId(Long stageId);
-	
-	Optional<Booking> getBookingsByToken(String token);
-	
-	List<Booking> getBookingsByStaffId(Long staffId);
-}
+//package com.example.demo.service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import com.example.demo.entity.Booking;
+//
+//public interface BookingService {
+//	
+//	void createBooking(Booking booking);
+//	
+//	void updateBooking(Booking booking);
+//	
+//	void deleteBooking(Long Id);
+//	
+//	List<Booking> getAllBookings();
+//	
+//	List<Booking> getBookingsById(Long Id);
+//	
+//	List<Booking> getBookingsByName(String name);
+//	
+//	List<Booking> getBookingsByEmail(String email);
+//	
+//	List<Booking> getBookingsByStageId(Long stageId);
+//	
+//	Optional<Booking> getBookingsByToken(String token);
+//	
+//	List<Booking> getBookingsByStaffId(Long staffId);
+//}
--- src/main/java/com/example/demo/service/impl/BookingServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BookingServiceImpl.java
@@ -1,37 +1,39 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.entity.Booking;
-import com.example.demo.service.BookingService;
-
-public class BookingServiceImpl implements BookingService {
-	
-	@Override
-	public void createBooking(Booking booking) {}
-	
-	@Override
-	public void updateBooking(Booking booking) {}
-	
-	@Override
-	public void deleteBooking(Long Id) {}
-	
-	@Override
-	public List<Booking> getAllBookings() {}
-	
-	@Override
-	public List<Booking> getBookingsById(Long Id) {}
-	
-	@Override
-	public List<Booking> getBookingsByName(String name) {}
-	
-	@Override
-	public List<Booking> getBookingsByEmail(String email) {}
-	
-	@Override
-	public List<Booking> getBookingsByStageId(Long stageId) {}
-	
-	@Override
-	public Optional<Booking> getBookingsByToken(String token) {}
-	
-	@Override
-	public List<Booking> getBookingsByStaffId(Long staffId) {}	
-}
+//package com.example.demo.service.impl;
+//
+//import java.util.List;
+//
+//import com.example.demo.entity.Booking;
+//import com.example.demo.service.BookingService;
+//
+//public class BookingServiceImpl implements BookingService {
+//	
+//	@Override
+//	public void createBooking(Booking booking) {}
+//	
+//	@Override
+//	public void updateBooking(Booking booking) {}
+//	
+//	@Override
+//	public void deleteBooking(Long Id) {}
+//	
+//	@Override
+//	public List<Booking> getAllBookings() {}
+//	
+//	@Override
+//	public List<Booking> getBookingsById(Long Id) {}
+//	
+//	@Override
+//	public List<Booking> getBookingsByName(String name) {}
+//	
+//	@Override
+//	public List<Booking> getBookingsByEmail(String email) {}
+//	
+//	@Override
+//	public List<Booking> getBookingsByStageId(Long stageId) {}
+//	
+//	@Override
+//	public Optional<Booking> getBookingsByToken(String token) {}
+//	
+//	@Override
+//	public List<Booking> getBookingsByStaffId(Long staffId) {}	
+//}
