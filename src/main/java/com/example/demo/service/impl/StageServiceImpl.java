b159e4971882e98b9195a7bbd3d5f6267d0ec862
小林瑞歩 <mizuho.kobayashi@nkaihatsu.com>
2026-09-17 23:23:16 +0900

    update: StageServiceImplにアノテーションの追加

--- src/main/java/com/example/demo/service/impl/StageServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StageServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.repository.StageRepository;
 import com.example.demo.service.StageService;
 
+@Service
 public class StageServiceImpl implements StageService {
 
 	@Autowired
