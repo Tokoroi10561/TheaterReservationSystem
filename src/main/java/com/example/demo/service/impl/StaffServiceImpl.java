47ef65cea7d0ecf237822be42c239297889630d6
小林瑞歩 <mizuho.kobayashi@nkaihatsu.com>
2026-09-17 23:19:24 +0900

    add: StageServiceとアノテーション忘れのクラスにアノテーションの追加

--- src/main/java/com/example/demo/service/impl/StaffServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StaffServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.demo.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.repository.StaffRepository;
 import com.example.demo.service.StaffService;
 
+@Service
 public class StaffServiceImpl implements StaffService {
 
 	private StaffRepository staffRepositroy;
